f9419319c05564e032a84caef35c803eff4d08d2
sruthisagar <sruthidingu@gmail.com>
2021-02-23 11:31:13 +0530

    used pointer to pointer

--- 13-Polynomial Addition (Linked List).c
+++ 13-Polynomial Addition (Linked List).c
@@ -1,210 +1,174 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-struct poly
+struct polynomial
 {
-  int exp;
-  int coef;
-  struct poly *next;
+    int coef;
+    int exp;
+    struct polynomial *next;
 };
-typedef struct poly node;
-node *a = NULL, *b = NULL, *cc = NULL;
-void
-create (int i, int co, int ex, int w)
-{
-  node *k, *c;
-  k = (node *) malloc (sizeof (node));
-  k->coef = co;
-  k->exp = ex;
-  k->next = NULL;
-  if (w == 1)
-    {
-      if (i == 0)
-  {
-    a = k;
-  }
-      else
-  {
-    c = a;
-    while (c->next != NULL)
-      {
-        c = c->next;
-      }
-    c->next = k;
-  }
-    }
-  else
-    {
-      if (i == 0)
-  {
-    b = k;
-  }
-      else
-  {
-    c = b;
-    while (c->next != NULL)
-      {
-        c = c->next;
-      }
-    c->next = k;
-  }
-    }
 
-}
+typedef struct polynomial node;
 
-void
-sort (int i)
+void insert(int co, int ex, node **poly)
 {
-  node *h, *t1, *t2;
-  switch (i)
+    node *newNode, *ptr;
+
+    newNode = (node *)malloc(sizeof(node));
+    if (newNode == NULL)
+        printf("Failed to allocate memory\n");
+    else
     {
-    case 1:
-      h = a;
-      break;
-    case 2:
-      h = b;
-      break;
-    }
-  for (t1 = h; t1->next != NULL; t1 = t1->next)
-    {
-      for (t2 = t1->next; t2 != NULL; t2 = t2->next)
-  {
-    if (t2->exp > t1->exp)
-      {
-        int k;
-        k = t1->exp;
-        t1->exp = t2->exp;
-        t2->exp = k;
-        k = t1->coef;
-        t1->coef = t2->coef;
-        t2->coef = k;
-      }
-  }
+        newNode->coef = co;
+        newNode->exp = ex;
+        newNode->next = NULL;
+
+        if (*poly == NULL)
+            *poly = newNode;
+        else
+        {
+            ptr = *poly;
+            while (ptr->next != NULL) // point ptr to last node
+                ptr = ptr->next;
+
+            ptr->next = newNode;
+        }
     }
 }
 
-void
-add ()
+void sort(node *poly)
 {
-  node *c, *p, *q, *d;
-  p = a;
-  q = b;
-  c = (node *) malloc (sizeof (node));
-  d = c;
-  while (p != NULL && q != NULL)
+    node *ptr1, *ptr2;
+    int temp;
+
+    for (ptr1 = poly; ptr1 != NULL; ptr1 = ptr1->next)
     {
-      node *k = (node *) malloc (sizeof (node));
-      if (p->exp > q->exp)
-  {
-    k->coef = p->coef;
-    k->exp = p->exp;
-    k->next = NULL;
-    d->next = k;
-    d = d->next;
-    p = p->next;
-  }
-      else if (p->exp < q->exp)
-  {
-    k->coef = q->coef;
-    k->exp = q->exp;
-    k->next = NULL;
-    d->next = k;
-    d = d->next;
-    q = q->next;
-  }
-      else
-  {
-    int u = 0;
-    u = p->coef + q->coef;
-    if (u != 0)
-      {
-        k->coef = u;
-        k->exp = p->exp;
-        k->next = NULL;
-        d->next = k;
-        d = d->next;
-      }
-    p = p->next;
-    q = q->next;
-  }
+        for (ptr2 = ptr1->next; ptr2 != NULL; ptr2 = ptr2->next)
+        {
+            if (ptr2->exp > ptr1->exp)
+            {
+                temp = ptr1->exp;
+                ptr1->exp = ptr2->exp;
+                ptr2->exp = temp;
+
+                temp = ptr1->coef;
+                ptr1->coef = ptr2->coef;
+                ptr2->coef = temp;
+            }
+        }
     }
-  while (p != NULL)
-    {
-      node *k = (node *) malloc (sizeof (node));
-      k->coef = p->coef;
-      k->exp = p->exp;
-      k->next = NULL;
-      d->next = k;
-      d = d->next;
-      p = p->next;
-    }
-  while (q != NULL)
-    {
-      node *k = (node *) malloc (sizeof (node));
-      k->coef = q->coef;
-      k->exp = q->exp;
-      k->next = NULL;
-      d->next = k;
-      d = d->next;
-      q = q->next;
-    }
-  d->next = NULL;
-  cc = c->next;
 }
 
-void
-display (int i)
+void add(node *ptr1, node *ptr2, node **result)
 {
-  node *k;
-  if (i == 1)
+    int sum;
+
+    node *sumNode = (node *)malloc(sizeof(node));
+    sumNode->next = NULL;
+    *result = sumNode;
+
+    while (ptr1 != NULL && ptr2 != NULL)
     {
-      k = a;
+        if (ptr1->exp > ptr2->exp)
+        {
+            sumNode->coef = ptr1->coef;
+            sumNode->exp = ptr1->exp;
+            ptr1 = ptr1->next;
+        }
+        else if (ptr1->exp < ptr2->exp)
+        {
+            sumNode->coef = ptr2->coef;
+            sumNode->exp = ptr2->exp;
+            ptr2 = ptr2->next;
+        }
+        else
+        {
+            sum = ptr1->coef + ptr2->coef;
+            if (sum != 0)
+            {
+                sumNode->coef = sum;
+                sumNode->exp = ptr1->exp;
+            }
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+
+        if (ptr1 != NULL && ptr2 != NULL)
+        {
+            sumNode->next = (node *)malloc(sizeof(node));
+            sumNode = sumNode->next;
+            sumNode->next = NULL;
+        }
     }
-  else if (i == 2)
+    while (ptr1 != NULL || ptr2 != NULL)
     {
-      k = b;
+        sumNode->next = (node *)malloc(sizeof(node));
+        sumNode = sumNode->next;
+        sumNode->next = NULL;
+
+        if (ptr1 != NULL)
+        {
+            sumNode->coef = ptr1->coef;
+            sumNode->exp = ptr1->exp;
+            ptr1 = ptr1->next;
+        }
+        if (ptr2 != NULL)
+        {
+            sumNode->coef = ptr2->coef;
+            sumNode->exp = ptr2->exp;
+            ptr2 = ptr2->next;
+        }
     }
-  else
-    k = cc;
-  while (k->next != NULL)
-    {
-      if (k->coef != 0)
-  printf ("%ix^%i + ", k->coef, k->exp);
-      k = k->next;
-    }
-    if (k->coef != 0)
-      printf ("%ix^%i", k->coef, k->exp);
-  printf ("\n");
 }
 
-void
-main ()
+void display(node *poly)
 {
-  node *k;
-  int n, n2, ex, co;
-  printf ("Enter the number of terms in first polynomial\n");
-  scanf ("%i", &n);
-  printf ("\nEnter the first polynomial\n");
-  for (int i = 0; i < n; i++)
+    if (poly != NULL)
     {
-      scanf ("%i %i", &co, &ex);
-      create (i, co, ex, 1);
+        while (poly->next != NULL)
+        {
+            if (poly->coef != 0)
+                printf("%ix^%i + ", poly->coef, poly->exp);
+            poly = poly->next;
+        }
+        if (poly->coef != 0)
+            printf("%ix^%i", poly->coef, poly->exp);
+        printf("\n");
     }
-  printf ("\nEnter the number of terms in second polynomial\n");
-  scanf ("%i", &n2);
-  printf ("\nEnter the second polynomial\n");
-  for (int i = 0; i < n2; i++)
+}
+
+void main()
+{
+    int n1, n2, coef, exp;
+    node *poly1 = NULL, *poly2 = NULL, *sumPoly = NULL;
+
+    printf("Enter the number of terms in first polynomial\n");
+    scanf("%i", &n1);
+    printf("\nEnter the first polynomial\n");
+    for (int i=0; i<n1; i++)
     {
-      scanf ("%i %i", &co, &ex);
-      create (i, co, ex, 2);
+        scanf("%i %i", &coef, &exp);
+        insert(coef, exp, &poly1);
     }
-  sort (1);
-  sort (2);
-  printf ("\nFirst polynomial:\n");
-  display (1);
-  printf ("\nSecond polynomial:\n");
-  display (2);
-  printf ("\nSum of the two polynomials:\n");
-  add ();
-  display (3);
+
+    printf("\nEnter the number of terms in second polynomial\n");
+    scanf("%i", &n2);
+    printf("\nEnter the second polynomial\n");
+    for (int i=0; i<n2; i++)
+    {
+        scanf("%i %i", &coef, &exp);
+        insert(coef, exp, &poly2);
+    }
+
+    sort(poly1);
+    sort(poly2);
+
+    printf("\nFirst polynomial:\n");
+    display(poly1);
+    printf("\nSecond polynomial:\n");
+    display(poly2);
+
+    printf("\nSum of the two polynomials:\n");
+    add(poly1, poly2, &sumPoly);
+    display(sumPoly);
 }
